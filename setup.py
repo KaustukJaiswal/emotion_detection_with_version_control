a63acdc93dfe9326261ba3e928edee01917c940c
KaustukJaiswal <kaustubhjaiswal778@gmail.com>
2026-01-14 11:03:26 +0530

    add cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project to detect the emotion from the text',
+    author='Kaustuk',
+    license='',
+)
